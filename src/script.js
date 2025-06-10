1f948c9141e8074f24902b94dac769ac1735f092
HeyyKamleshh <hiikamleshh@gmail.com>
2025-06-10 20:20:39 +0530

    updated three.js bundles

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// New, working jsDelivr URLs
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/controls/OrbitControls.js';
 
 const controlButton = document.getElementById('control_button');
 const rotate = document.getElementById('rotate_table');
